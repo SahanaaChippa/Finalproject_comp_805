bf21afb6a7310bb3b2d1c38665bafe05b6c02251
Sahanaa Chippa <sahanaa.chippa@unh.edu>
2024-05-14 17:58:37 -0500

    add an image in the homepage

--- usauniversities/usauniversities/Myapp/views.py
+++ usauniversities/usauniversities/Myapp/views.py
@@ -1,3 +1,11 @@
-from django.shortcuts import render
+import requests
+from django.shortcuts import render, redirect, get_object_or_404
+from django.http import HttpResponse
+from django.contrib.auth.decorators import login_required
+from .models import University, Review, SavedUniversity
+from .forms import ReviewForm
+from django.http import JsonResponse
 
-# Create your views here.
+def home(request):
+    picture_path = r'usauniversity\usauniversity\static\images\image.jpg'  # Update this with the actual path to your picture
+    return render(request, 'home.html', {r'usauniversity\usauniversity\static\images\image.jpg': picture_path})

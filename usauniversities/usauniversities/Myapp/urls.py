37e8fa971bbaf7bcc0974fb0c74334c41d64da52
Sahanaa Chippa <sahanaa.chippa@unh.edu>
2024-05-16 00:39:15 -0500

    add university list urls

--- usauniversities/usauniversities/Myapp/urls.py
+++ usauniversities/usauniversities/Myapp/urls.py
@@ -3,3 +3,12 @@ from django.urls import path  # Import the view function
 from . import views
 from django.conf.urls.static import static
 from .views import home
+
+urlpatterns = [
+    # Other URL patterns...
+    path('', home, name='home'),
+    path('university/', views.universities_list, name='universitie_list'),
+    path('universities/<int:university_id>/reviews/', views.university_detail, name='university_reviews'),
+    path('review/<int:review_id>/update/', views.review_update, name='review_update'),
+
+]
